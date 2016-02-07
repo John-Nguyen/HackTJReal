0b52c91e7b550142f454236291e067533e03be2f
John Nguyen <jnguyen38@gmail.com>
2016-02-07 04:39:14 -0500

    dix

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -46,20 +46,4 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
-
-    function scan(){
-        var scanner = cordova.require("cordova/plugin/barcodescanner"); 
-        scanner.scan(scannerSuccess, scannerFailure);
-        /*cordova.plugins.barcodeScanner.scan(
-          function (result) {
-              alert("We got a barcode\n" +
-                    "Result: " + result.text + "\n" +
-                    "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);
-          }, 
-          function (error) {
-              alert("Scanning failed: " + error);
-          }
-        );*/
-    }
 };
---
+++ platforms/browser/www/nessie-javascript-sdk-master/lib/customer.js
@@ -0,0 +1,110 @@
+define('customer', function (require) {
+    "use strict";
+    var Config = require('capital_one');
+    
+	var Customer = {
+		initWithKey: function(apiKey) {
+			Config.setApiKey(apiKey);
+			return this;
+		},
+		urlWithEntity: function() {
+			return Config.baseUrl+"/customers/";
+		},
+		urlWithAcctEntity: function() {
+			return Config.baseUrl+"/accounts/";
+		},
+		apiKey: function() {
+			return Config.apiKey;
+		},
+		/**
+		  # @Method: getCustomers
+		  # @Brief: Gets all customers the API key has acccess to.
+		  # @Returns an array of JSON Objects.
+		**/
+		getCustomers: function() {
+			var customers;
+			var request = $.ajax({ 
+				url: this.urlWithEntity(), 
+				data: 'key='+this.apiKey(), 
+				async: false,
+				dataType: 'json'
+			});
+
+			request.done(function(results) {
+				customers = results;
+			});
+			return customers;
+		},
+		/**
+		  # @Method: getCustomerById
+		  # @Brief: Gets the specified customer's information.
+		  # @Parameters: CustomerId
+		  # @Returns a object with the customer data
+		**/
+		getCustomerById: function(custId) {
+			var customer;
+			var request = $.ajax({ 
+				url: this.urlWithEntity()+custId,
+				data: 'key='+this.apiKey(),
+				async: false,
+				dataType: 'json'
+			});
+
+			request.done(function(results) {
+				customer = results;
+			});
+			return customer;
+		},
+		/**
+		  # @Method: Get the customer for the given account.
+		  # @Parameters: AccountId
+		  # @Returns a object with the specified customer data.
+		**/
+		getCustomerByAcountId: function(accId) {
+			var customer;
+			var request = $.ajax({ 
+				url: this.urlWithAcctEntity()+accId+'/customer',
+				data: 'key='+this.apiKey(),
+				async: false,
+				dataType: 'json'
+			});
+			
+			request.done(function(results) {
+				customer = results;
+			});
+			return customer;
+		},
+		/**
+		  # @Method: updateCustomer
+		  # @Brief: Updates a customer by id with given JSON data. 
+		  # @Parameters: CustomerId, Customerobject.
+		  # @Note: Json is as follows: 
+		  #  {
+		  #   "address": {
+		  #     "street_number": "",
+		  #     "street_name": "",
+		  #     "city": "",
+		  #     "state": "",
+		  #     "zip": ""
+		  #   }
+		  # }
+		  # @Returns http response code. 
+		**/
+		updateCustomer: function(custId, json) {
+			var respCode;
+			var request = $.ajax({
+				url: this.urlWithEntity()+custId+'?key='+this.apiKey(),
+				data: json,
+				contentType: 'application/json',
+				async: false,
+				type: 'PUT'
+			});
+
+			request.complete(function(jqXHR, textStatus) {
+				respCode = jqXHR.status;
+			});
+			return respCode;
+		}
+	};
+	return Customer;
+});
